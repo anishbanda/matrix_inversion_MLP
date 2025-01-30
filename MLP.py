1a0872281de14900591ee2a4519f5cc652a39e94
Anish Banda <bandaanish@gmail.com>
2025-01-30 15:58:53 -0500

    Added dropout layers to reduce overfitting

--- MLP.py
+++ MLP.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 
 class MLP(nn.Module):
     
@@ -16,24 +17,27 @@ class MLP(nn.Module):
         self.fc5 = nn.Linear(hidden_size, hidden_size) # Fifth hidden layer
         self.fc6 = nn.Linear(hidden_size, output_size) # Output layer
         
-        # Define activation function
-        self.relu = nn.LeakyReLU(negative_slope=0.01) # Allows small negative values
+        self.dropout = nn.Dropout(0.2) # Dropout to reduce overfitting
         
     def forward(self, x):
         
-        x = self.relu(self.fc1(x)) # Apply ReLU to first hidden layer
-        x = self.relu(self.fc2(x)) # Apply ReLU to second hidden layer
-        x = self.relu(self.fc3(x)) # Apply ReLU to third hidden layer
-        x = self.relu(self.fc4(x)) # Apply ReLU to fourth hidden layer
-        x = self.relu(self.fc5(x)) # Apply ReLU to fifth hidden layer
-        x = self.fc6(x) # Output layer (no activation
+        x = F.leaky_relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.leaky_relu(self.fc2(x))
+        x = self.dropout(x) 
+        x = F.leaky_relu(self.fc3(x))
+        x = self.dropout(x)
+        x = F.leaky_relu(self.fc4(x))
+        x = self.dropout(x)
+        x = F.leaky_relu(self.fc5(x))
+        x = self.fc6(x)
         
         return x
     
 # Testing the model with dummy input
 n = 3 # Matrix size (n x n)
 input_size = n * n
-hidden_size = 256
+hidden_size = 128
 output_size = n * n
 
 model = MLP(input_size, hidden_size, output_size)
