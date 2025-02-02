7e548df9315d668da20c171726f3ca39caf164a0
Anish Banda <bandaanish@gmail.com>
2025-02-02 16:56:33 -0500

    Simplified the model to have 3 hidden layers rather than 5

--- MLP.py
+++ MLP.py
@@ -12,10 +12,7 @@ class MLP(nn.Module):
         # Define fully connected layers
         self.fc1 = nn.Linear(input_size, hidden_size) # First hidden layer
         self.fc2 = nn.Linear(hidden_size, hidden_size) # Second hidden layer
-        self.fc3 = nn.Linear(hidden_size, hidden_size) # Third hidden layer
-        self.fc4 = nn.Linear(hidden_size, hidden_size) # Fourth hidden layer
-        self.fc5 = nn.Linear(hidden_size, hidden_size) # Fifth hidden layer
-        self.fc6 = nn.Linear(hidden_size, output_size) # Output layer
+        self.fc3 = nn.Linear(hidden_size, output_size) # Output layer
         
         # Batch Normalization Layers
         # self.bn1 = nn.BatchNorm1d(hidden_size)
@@ -33,19 +30,14 @@ class MLP(nn.Module):
         x = self.dropout(x)
         x = F.leaky_relu(self.fc2(x))
         x = self.dropout(x) 
-        x = F.leaky_relu(self.fc3(x))
-        x = self.dropout(x)
-        x = F.leaky_relu(self.fc4(x))
-        x = self.dropout(x)
-        x = F.leaky_relu(self.fc5(x))
-        x = self.fc6(x)
+        x = self.fc3(x)
         
         return x
     
 # Testing the model with dummy input
 n = 3 # Matrix size (n x n)
 input_size = n * n
-hidden_size = 128
+hidden_size = 64
 output_size = n * n
 
 model = MLP(input_size, hidden_size, output_size)
